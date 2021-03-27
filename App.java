74e605b85a7a6d1062579abd6bd062c6a320aaaa
Sojeong Sim <sowjd1225@gmail.com>
2021-03-27 18:53:39 +0900

    Add result

--- App.java
+++ App.java
@@ -14,3 +14,25 @@ public class App {
         myRamyeon.cook();
     }
 }
+
+/*
+실행결과
+
+<<This is original Ramyeon recipe>>
+Put Water 500ml.
+Boiling...
+Put noodle.
+Put whole soup powder.
+Boiling for 2.0 minutes...
+Finish! Enjoy your Ramyeon:)
+
+<<This is how I cook Ramyeon>>
+Put Water 500ml.
+Boiling...
+Put noodle.
+Put 90% of soup powder.
+Put egg.
+Put green onion.
+Boiling for 1.5 minutes...
+Finish! Enjoy your Ramyeon:)
+*/
