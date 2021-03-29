1107e8026e62c2539950d337c192164dda560522
Sojeong Sim <sowjd1225@gmail.com>
2021-03-29 19:59:05 +0900

    Update App.java

--- App.java
+++ App.java
@@ -6,11 +6,12 @@ public class App {
 
         System.out.println("\n<<This is how I cook Ramyeon>>");
         CustomRamyeon myRamyeon = new CustomRamyeon();
+        myRamyeon.setWater(450);
         myRamyeon.setSoupPowder(90);
-        myRamyeon.setTimeToBoil(1.5);
+        myRamyeon.setBoilTime(1.5);
         myRamyeon.addIngredient("egg");
         myRamyeon.addIngredient("green onion");
-
+        myRamyeon.addIngredient("kimchi");
         myRamyeon.cook();
     }
 }
@@ -19,20 +20,17 @@ public class App {
 실행결과
 
 <<This is original Ramyeon recipe>>
-Put Water 500ml.
+Put Water 500ml
 Boiling...
-Put noodle.
-Put whole soup powder.
+Put noodle and soup powder
 Boiling for 2.0 minutes...
 Finish! Enjoy your Ramyeon:)
 
 <<This is how I cook Ramyeon>>
-Put Water 500ml.
+Put Water 450ml
 Boiling...
-Put noodle.
-Put 90% of soup powder.
-Put egg.
-Put green onion.
+Put noodle and 90% of soup powder
+Put egg, green onion and kimchi
 Boiling for 1.5 minutes...
 Finish! Enjoy your Ramyeon:)
 */
